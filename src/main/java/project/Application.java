b97650086de122e89364f74bcc411e07b791672a
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2022-04-16 00:21:24 +0900

    feat : Add ping.

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -3,23 +3,11 @@ package project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling 
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-}
-
-@CrossOrigin("*")
-@RestController
-private class HealthCheckController {
-    @GetMapping("/ping")
-    public String healthCheck() {
-        return "pong";
     }
 }
